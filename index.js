fd26d26070f1d52c0a3000e1b9cd9da112b4323d
Ajay Titus <ajaytitus1386@gmail.com>
2022-09-06 15:03:37 +0530

    feat: add entry script

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import InitiateMongoServer from "./config/db.js";
+import userRouter from "./routes/user.js";
+import planRouter from "./routes/plan.js";
+import subRouter from "./routes/subscription.js";
+
+InitiateMongoServer();
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is up and running!" });
+});
+
+// User Authentication Rotues
+app.use("/user", userRouter);
+
+// Plans Details
+app.use("/plans", planRouter);
+
+// Subscription Routes
+app.use("/subscriptions", subRouter);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at Port ${PORT}`);
+});
